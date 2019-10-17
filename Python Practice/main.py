f47c652ceb9f7e3dec1e22f2dd07bbd5faa86e4f
LE (Lee) <lm61613@gmail.com>
2019-10-17 13:52:02 -0500

    data frame and bar

--- Python Practice/main.py
+++ Python Practice/main.py
@@ -12,4 +12,24 @@ print(data)
 # or
 print(data[9:30])
 
+petal_data = data[['petal_length', 'petal_width','species']]
+print(petal_data)
+
+
+length = petal_data[['petal_length']
+width = petal_data[['petal_width']]
+species = petal_data[['species']]
+print(length)
+print(width)
+print(species)
+
+
+
+
+fig, ax = plt.subplots()
+df = pd.DataFrame({'petal_length': length, 'petal_width': width }, index= species)
+print(df)
+
+ax = df.plot.bar(rot=0)
+
 
